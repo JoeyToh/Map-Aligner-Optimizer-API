484c2064d79082f3fafe3611c22c86c6c6e2eba7
JoeyToh <joeytoh29@gmail.com>
2021-07-22 13:37:39 +0800

    Add api

---
+++ scripts/CPPConnector.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Bool
+
+class CPPConnector():
+    def __init__(self):
+        self.talker()
+
+    def talker(self):
+        pub = rospy.Publisher('chatter', Bool, queue_size=10)
+        rospy.init_node('talker', anonymous=True)
+        rate = rospy.Rate(10) # 10hz
+
+        bool = True
+        rospy.loginfo(bool)
+        pub.publish(bool)
